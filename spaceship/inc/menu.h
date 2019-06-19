195e20bcb9e229d04eb40aa9048e551596640ecc
Martin Jensen <mjensen1231992@gmail.com>
2019-06-19 10:17:30 +0200

    Create menu module

---
+++ spaceship/inc/menu.h
@@ -0,0 +1,7 @@
+#include <stdint.h>
+#include <stdio.h>
+#include "lcd_driver.h"
+#include "trig_lut.h"
+#include "30010_io.h"
+#include "charset.h"
+#define ESC 0x1B
---
+++ spaceship/src/menu.c
@@ -0,0 +1,8 @@
+#include <stdint.h>
+#include <stdio.h>
+#include "lcd_driver.h"
+#include "trig_lut.h"
+#include "30010_io.h"
+#include "charset.h"
+#include "menu.h"
+#define ESC 0x1B
