5974190460efae97b8413d1adce5b341899906fc
Magnus Oksbøl Therkelsen <magnus@thrklsn.dk>
2019-06-20 11:33:58 +0200

    Clean up main file

--- spaceship/src/main.c
+++ spaceship/src/main.c
@@ -20,64 +20,15 @@
 #include "trig_lut.h"
 #include "lcd_driver.h"
 #include <wchar.h>
+#include <spaceship.h>
 
 
 int main(void)
 {
-    uint16_t length = 1;
-    char out[length+1];
-
-    uart_init(916000);
-    //clrscr();
-    //gotoxy(1,1);
-    int8_t *buffer [512];
-    //gotoxy(1,1);
-//    int8_t *buffer [512];
-//
-//    lcd_init();
-//
-//    memset(buffer,0x00,512);
-//
-//    lcdWriteString("Start press 1", buffer,0,0);
-//
-//    lcd_push_buffer(buffer);
-
-//    lcd_init();
-//    memset(buffer,0x00,512);
-//    lcdWriteString("First I wrote this line", buffer,1,0);
-//    lcdWriteString("Then this one", buffer,0,5);
-//    lcd_push_buffer(buffer);
-    uint32_t x=10;
-    uint32_t y=10;
-    char next;
-    uint8_t heading = 0;
-
     initGame();
 
-
-
     while(1){
-
-
-
-    //gotoxy(x++,y++);
-
-
-
-
-
-
-    clrscr();
-
-//    heading += rotateSpaceship(heading);
-//    heading %= 8;
-
-
-//    drawSpaceShip(x,y,heading);
-    drawGame();
-
-
-
-
+        updateGame();
+        drawGame();
     }
 }
