b01d345d60c1c319e2ffb12a038ade2dc0cfcb2a
Magnus Oksbøl Therkelsen <magnus@thrklsn.dk>
2019-06-25 14:24:54 +0200

    Updated HUD gave the ship life and ammo

--- spaceship/src/HUD.c
+++ spaceship/src/HUD.c
@@ -3,13 +3,14 @@
 static char buffer[512] = {0};
 
 void updateHUD(Player * player){
-    //char heart[3] = {0x5f + 0x20, 0x60 + 0x20, 0x5f + 0x20, 0x60 + 0x20, 0x5f + 0x20, 0x60 + 0x20, '\0'};
-    //lcdWriteString(heart, buffer, 0, 0);
+    uint8_t i;
+    for(i = 0; i < (*player).hp; i++){
+        lcdWriteSymbol(95,buffer,0,i*2);
+        lcdWriteSymbol(96,buffer,0,i*2+1);
+    }
 
-
-    uint32_t i;
     for(i = 0; i < (*player).ammo; i++){
-        lcdWriteSymbol(97,buffer,i,3);
+        lcdWriteSymbol(97,buffer,2,i);
     }
     lcd_push_buffer(buffer);
 }
