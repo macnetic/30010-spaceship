1959e840657da0f4352e8b691b76b260d7abf302
s164026 <s>
2019-06-25 16:01:19 +0200

    Stuffs

--- spaceship/src/HUD.c
+++ spaceship/src/HUD.c
@@ -3,16 +3,27 @@
 static char buffer[512] = {0};
 
 void updateHUD(Player * player){
+    //Remove old drawings
     memset(buffer,0x00,512);
 
     uint8_t i;
+    //How many lives the player has left
     for(i = 0; i < (*player).hp; i++){
         lcdWriteSymbol(95,buffer,0,i*2);
         lcdWriteSymbol(96,buffer,0,i*2+1);
     }
 
+    //How much ammo the player has left
     for(i = 0; i < (*player).ammo; i++){
-        lcdWriteSymbol(97,buffer,2,i);
+        lcdWriteSymbol(97,buffer,1,i);
     }
+
+    //Do something here
+    lcdWriteString("No powerups :(",buffer,3,0);
+
+    //Show score
+    lcdWriteString("Score: applesauce",buffer,3,0);
+
+    //Update display
     lcd_push_buffer(buffer);
 }
