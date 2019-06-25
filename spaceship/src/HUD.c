a859b9ef1cda5bdfb21c57c68f3cb56c270da600
s164026 <s>
2019-06-25 09:52:14 +0200

    Ammo

--- spaceship/src/HUD.c
+++ spaceship/src/HUD.c
@@ -4,7 +4,11 @@ static char buffer[512] = {0};
 
 void updateHUD(Player * player){
     char heart[3] = {0x5f + 0x20, 0x60 + 0x20, 0x5f + 0x20, 0x60 + 0x20, 0x5f + 0x20, 0x60 + 0x20, '\0'};
-
     lcdWriteString(heart, buffer, 0, 0);
+
+    uint8_t i;
+    for(i = 0; i < (*player).ammo; i++){
+        lcdWriteSymbol(97,buffer,3,i);
+    }
     lcd_push_buffer(buffer);
 }
